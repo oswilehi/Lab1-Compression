0ed559d496210f349ebf57c622f6842e607f0085
oswilehi <oswilehi@gmail.com>
2017-09-20 01:13:01 -0600

    Added library that we created

---
+++ CompressionLibrary/Huffman.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab1_Compression
+{
+    class Huffman
+    {
+        /// <summary>
+        /// Atributes
+        /// </summary>
+        private HuffmanNode root;
+        public int sizeOriginalSize;
+
+        public int sizeCompressedFile;
+        private Dictionary<char, int> frequencies;
+        private int lengthOfPrefixCodes;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="filePath">File path</param>
+        public Huffman()
+        {
+            root = null;
+            frequencies = new Dictionary<char, int>();
+            sizeCompressedFile = 0;
+            sizeOriginalSize = 0;
+
+        }
+        /// <summary>
+        /// Method to start the process to compress the file
+        /// </summary>
+        /// <param name="path"></param>
+        public void Compression(string path)
+        {
+            ReadFrequencies(path);
+        }
+
+        /// <summary>
+        /// Method to compress file
+        /// </summary>
+        /// <param name="filepath">File path</param>
+        /// <param name="bytes">File byte array</param>
+        private void compression(string filepath, byte[] bytes)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), Path.GetFileNameWithoutExtension(filepath) + ".comp");
+            StreamWriter file = new StreamWriter(path);
+            file.WriteLine("1," + Path.GetFileName(filepath));
+            Dictionary<char, string> encode = PrefixCode();
+            foreach (KeyValuePair<char, string> item in encode)
+            {
+                if (item.Key == '\r') //salto de línea
+                    file.Write("13"); // No le podes poner una + o un -, imaginate que venga ese simbolo en la imagen?
+                else if (item.Key == '\n')//enter
+                    file.Write("10");
+                else
+                    file.Write(item.Key);
+
+                file.Write('-');
+
+
+                file.Write(item.Value);
+                file.Write('|');
+            }
+            file.WriteLine();
+            file.Flush();
+            file.Close();
+            using (var outputFile = new FileStream(path, FileMode.Append))
+            {
+                using (var writer = new BinaryWriter(outputFile, Encoding.ASCII))
+                {
+                    string pivot = "";
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        pivot += encode[(char)bytes[i]];
+                    }
+                    lengthOfPrefixCodes = pivot.Length;
+                    var bit = 0;
+
+                    for (int i = 0; i < pivot.Length; i++)
+                    {
+                        if (pivot.Length - i >= 8)
+                        {
+                            string current = pivot.Substring(i, 8);
+                            bit = Convert.ToInt16(current, 2);
+                            i = i + 7;
+                        }
+                        else
+                        {
+                            string current = pivot.Substring(i, (pivot.Length % 8));
+                            bit = Convert.ToInt16(current, 2);
+                            i = pivot.Length;
+                        }
+
+                        byte bits = (byte)int.Parse(bit.ToString());
+                        writer.Write(bits);
+                    }
+                    sizeCompressedFile = (int)outputFile.Length;
+                }
+            }
+        }
+        /// <summary>
+        /// Creat a dictionary (char,int) with the frequencies of each character
+        /// </summary>
+        /// <param name="filepath"></param>
+        private void ReadFrequencies(string filepath)
+        {
+            using (var file = new FileStream(filepath, FileMode.Open))
+            {
+                using (var binaryFile = new BinaryReader(file))
+                {
+                    var bytes = binaryFile.ReadBytes((int)file.Length);
+                    sizeOriginalSize = (int)file.Length;
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        if (!frequencies.ContainsKey((char)bytes[i]))
+                            frequencies.Add((char)bytes[i], 0);
+                        frequencies[(char)bytes[i]]++;
+
+                    }
+                    HuffmanTree(frequencies);
+                    compression(filepath, bytes);
+                }
+            }
+        }
+        /// <summary>
+        /// Get the add of two probabilities
+        /// </summary>
+        /// <param name="n1">frequency number one</param>
+        /// <param name="n2">frequency number one</param>
+        /// <returns>The sum of both frequencies</returns>
+        private double SumOfProbabilities(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        /// <summary>
+        /// Creat Huffman tree 
+        /// </summary>
+        /// <param name="frequencies">Dictionary of frequencies</param>
+        private void HuffmanTree(IEnumerable<KeyValuePair<char, int>> frequencies)
+        {
+            HuffmanQueue<HuffmanNode> priorityQueue = new HuffmanQueue<HuffmanNode>();
+
+            foreach (KeyValuePair<char, int> item in frequencies)
+            {
+                priorityQueue.Queue(new HuffmanNode { value = item.Key, probability = GetProbability(item.Value) }, GetProbability(item.Value));
+            }
+            while (priorityQueue.Count > 1)
+            {
+                HuffmanNode n1 = priorityQueue.Dequeue();
+                HuffmanNode n2 = priorityQueue.Dequeue();
+                HuffmanNode newNode = new HuffmanNode { left = n1, right = n2, probability = SumOfProbabilities(n1.probability, n2.probability) };
+                n1.parent = newNode;
+                n2.parent = newNode;
+                priorityQueue.Queue(newNode, newNode.probability);
+            }
+            root = priorityQueue.Dequeue();
+
+        }
+        private double GetProbability(int value)
+        {
+            double v = value;
+            double s = sizeOriginalSize;
+            return v / s;
+        }
+        /// <summary>
+        /// Dictionary with the prefix code and the characters
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<char, string> PrefixCode()
+        {
+            Dictionary<char, string> prefixcode = new Dictionary<char, string>();
+            Encode(root, prefixcode, "");
+            return prefixcode;
+        }
+        /// <summary>
+        ///Method to encode
+        /// </summary>
+        /// <param name="node">Root</param>
+        /// <param name="prefixcode"></param>
+        /// <param name="prefix"></param>
+        private void Encode(HuffmanNode node, Dictionary<char, string> prefixcode, string prefix)
+        {
+            if (node.left != null)
+            {
+                Encode(node.left, prefixcode, prefix + "0");
+                Encode(node.right, prefixcode, prefix + "1");
+            }
+            else
+                prefixcode.Add((char)node.value, prefix);
+        }
+
+        public void Decompress(string path)
+        {
+            Dictionary<string, string> prefijos = new Dictionary<string, string>();
+            StreamReader reader = new StreamReader(path);
+            string firstLine = reader.ReadLine();
+            var infoOfFile = firstLine.Split(',');
+            string fileName = infoOfFile[1];
+            string secondLine = reader.ReadLine();
+            secondLine = secondLine.TrimEnd('|');
+            var separatedSecondLine = secondLine.Split('|');
+            int sizeToJump = firstLine.Length + secondLine.Length + 4 /* Ese cuatro es por los dos saltos de lineas y los dos enters*/ + 1 /* y ese uno es por el palito que se quita del final*/;
+
+            string fileInBinary = "";
+
+
+            for (int i = 0; i < separatedSecondLine.Length; i++)
+            {
+                var valueAndPrefix = separatedSecondLine[i].Split('-');
+                string value = valueAndPrefix[0];
+                string prefix = valueAndPrefix[1];
+                prefijos[prefix] = value;
+            }
+
+            reader.Close();
+
+            using (var file = new FileStream(path, FileMode.Open))
+            {
+                using (var currentFile = new BinaryReader(file))
+                {
+                    var bytes = currentFile.ReadBytes((int)file.Length);
+
+                    for (int i = sizeToJump; i < bytes.Length; i++)
+                    {
+                        string partOfFile = Convert.ToString(bytes[i], 2);
+                        if (partOfFile.Length < 8 && i != bytes.Length - 1)
+                            partOfFile = partOfFile.PadLeft(8, '0');
+                        else if (i == bytes.Length - 1)
+                            partOfFile = partOfFile.PadLeft(lengthOfPrefixCodes % 8, '0');
+                        fileInBinary += partOfFile;
+                    }
+                }
+            }
+
+            string pathToSave = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+            using (StreamWriter outputFile = new StreamWriter(pathToSave + fileName))
+            {
+                int start = 0;
+                for (int i = 0; i <= fileInBinary.Length; i++)
+                {
+
+                    if (prefijos.ContainsKey(fileInBinary.Substring(0, i)))
+                    {
+                        if (prefijos[fileInBinary.Substring(0, i)] == "13")
+                        {
+                            fileInBinary = fileInBinary.Remove(start, i);
+                            i = 0;
+                        }                         
+                        else if (prefijos[fileInBinary.Substring(0, i)] == "10")
+                        {
+                            outputFile.Write(Environment.NewLine);
+                            fileInBinary = fileInBinary.Remove(start, i);
+                            i = 0;
+                        }
+                        else
+                        {
+                            outputFile.Write(prefijos[fileInBinary.Substring(0, i)]);
+                            fileInBinary = fileInBinary.Remove(start, i);
+                            i = 0;
+                        }                  
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Gives the ratio of compression
+        /// </summary>
+        /// <returns></returns>
+        public double compressionRatio()
+        {
+            double sizeAfter = sizeCompressedFile;
+            double sizeBefore = sizeOriginalSize;
+            return Math.Round(sizeAfter / sizeBefore, 2);
+        }
+
+        /// <summary>
+        /// Gives the compression factor
+        /// </summary>
+        /// <returns></returns>
+        public double compressionFactor()
+        {
+            double sizeAfter = sizeCompressedFile;
+            double sizeBefore = sizeOriginalSize;
+            return Math.Round(sizeBefore / sizeAfter, 2);
+        }
+
+        /// <summary>
+        /// Gives the saving percentage
+        /// </summary>
+        /// <returns></returns>
+        public double savingPercentage()
+        {
+            double sizeAfter = sizeCompressedFile;
+            double sizeBefore = sizeOriginalSize;
+            return Math.Round((sizeBefore - sizeAfter) / sizeBefore * 100, 2);
+        }
+
+    }
+}
+
+
---
+++ CompressionLibrary/RLE.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab1_Compression
+{
+    class RLE
+    {
+        public static int sizeCompressedFile { get; set; }
+        public static int sizeOriginalSize { get; set; }
+        /// <summary>
+        /// This method compress a file with the RLE method
+        /// </summary>
+        /// <param name="pathFileToCompress"></param>
+        public static void compression(string pathFileToCompress)
+        {
+            StreamWriter outputfile = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), Path.GetFileNameWithoutExtension(pathFileToCompress)) + ".comp");
+            outputfile.WriteLine("0," + Path.GetFileName(pathFileToCompress));
+            outputfile.Flush();
+            outputfile.Close();
+            using (var file = new FileStream(pathFileToCompress, FileMode.Open))
+            {
+                sizeOriginalSize = (int)file.Length;
+                using (var reader = new BinaryReader(file))
+                {
+                    var bytes = reader.ReadBytes((int)file.Length);
+
+                    int counterOfEquals = 1;
+                    for (int i = 0; i < file.Length; i++)
+                    {
+                        if (i != file.Length - 1 && bytes[i] == bytes[i + 1])
+                        {
+                            counterOfEquals++;
+                        }
+                        else
+                        {
+                            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), Path.GetFileNameWithoutExtension(pathFileToCompress));
+                            using (var outputFile = new FileStream(path +".comp", FileMode.Append))
+                            {
+                                using (var writer = new BinaryWriter(outputFile, Encoding.ASCII))
+                                {
+                                    var counterOfEqualsInChar = (char)counterOfEquals;
+                                    writer.Write(counterOfEqualsInChar);
+                                    writer.Write(bytes[i]);
+                                    counterOfEquals = 1;
+                                    sizeCompressedFile = (int)outputFile.Length;
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// This method decompress a file that was compressed by RLE method
+        /// </summary>
+        /// <param name="filepath"></param>
+        public static void decompression(string filepath)
+        {
+            StreamReader reader1 = new StreamReader(filepath);
+            string firstLine = reader1.ReadLine();
+            var info = firstLine.Split(',');
+            string fileName = info[1];
+            int firsLineLength = firstLine.Length;
+            reader1.Close();
+            using (var file = new FileStream(filepath, FileMode.Open))
+            {
+                sizeCompressedFile = (int)file.Length;
+                using (var currentFile = new BinaryReader(file))
+                {
+                    var bytes = currentFile.ReadBytes((int)file.Length);
+                    int count = 0;
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+                    using (var outputFile = new FileStream(path + "\\D" + fileName, FileMode.Append))
+                    {
+                        using (var writer = new BinaryWriter(outputFile, Encoding.ASCII))
+                        {
+                            for (int i = firsLineLength + 2 /*Empiezo a leer despues de la linea donde esta la infoDelArchivo y el +2 es para saltarme el salto de linea y el ENTER*/; i < bytes.Length; i++)//fijo ya se que el primero es un número 
+                            {
+                                count = (int)bytes[i];
+                                while (count > 0)
+                                {
+                                    writer.Write(bytes[i + 1]);
+                                    count--;
+                                }
+                                i++;
+
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gives the ratio of compression
+        /// </summary>
+        /// <returns></returns>
+        public static double compressionRatio()
+        {
+            double sizeAfter = sizeCompressedFile;
+            double sizeBefore = sizeOriginalSize;
+            return Math.Round(sizeAfter / sizeBefore, 2);
+        }
+
+        /// <summary>
+        /// Gives the compression factor
+        /// </summary>
+        /// <returns></returns>
+        public static double compressionFactor()
+        {
+            double sizeAfter = sizeCompressedFile;
+            double sizeBefore = sizeOriginalSize;
+            return Math.Round( sizeBefore / sizeAfter, 2);
+        }
+
+        /// <summary>
+        /// Gives the saving percentage
+        /// </summary>
+        /// <returns></returns>
+        public static double savingPercentage()
+        {
+            double sizeAfter = sizeCompressedFile;
+            double sizeBefore = sizeOriginalSize;
+            return Math.Round((sizeBefore - sizeAfter) / sizeBefore * 100, 2);
+        }
+
+
+    }
+}
